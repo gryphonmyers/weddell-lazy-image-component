f1c010b4e9bb4e1cf6e0c0e0acd88a1cd1f4ec3b
Gryphon Myers <gryphon.myers@2k.com>
2019-05-06 16:23:46 +0800

    API updates and add for refresh from parent

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const defaults = require('defaults-es6/deep');
-const {mix} = require('mixwith');
+const { mix } = require('mixwith');
 const viewportCollisionMixin = require('weddell-viewport-collision-component-mixin');
 
 const LOAD_LIMIT = 3;
 var loadQueue = [];
 var done = [];
 
-function queueLoad(component) {
-    if (done.indexOf(component) > -1) {
-        component.state.canLoad = true;
+function queueLoad(state) {
+    if (done.indexOf(state) > -1) {
+        state.canLoad = true;
         return;
     }
-    if (loadQueue.indexOf(component) === -1) {    
-        loadQueue.push(component);
+    if (loadQueue.indexOf(state) === -1) {
+        loadQueue.push(state);
     }
     rotateQueue();
 }
 
-function resetDone(component) {
-    var i = done.indexOf(component);
-    if (done.indexOf(component) > -1) {
+function resetDone(state) {
+    var i = done.indexOf(state);
+    if (done.indexOf(state) > -1) {
         done.splice(i, 1);
     }
-    i = loadQueue.indexOf(component);
-    if (loadQueue.indexOf(component) > -1) {
+    i = loadQueue.indexOf(state);
+    if (loadQueue.indexOf(state) > -1) {
         loadQueue.splice(i, 1);
     }
-    component.state.canLoad = false;
+    state.canLoad = false;
     rotateQueue();
 }
 
@@ -34,22 +34,22 @@ function rotateQueue() {
     var i = 0;
 
     while (i < LOAD_LIMIT && i < loadQueue.length) {
-        loadQueue[i].state.canLoad = true;
+        loadQueue[i].canLoad = true;
         i++;
     }
 }
 
-function markDone(component) {
-    var i = loadQueue.indexOf(component);
+function markDone(state) {
+    var i = loadQueue.indexOf(state);
     if (i > -1) {
-        done.push(component);
+        done.push(state);
         loadQueue.splice(i, 1);
     }
     rotateQueue();
 }
 
-function unqueueLoad(component) {
-    var i = loadQueue.indexOf(component);
+function unqueueLoad(state) {
+    var i = loadQueue.indexOf(state);
     if (i > -1 && i >= LOAD_LIMIT) {
         loadQueue.splice(i, 1);
     }
@@ -57,92 +57,86 @@ function unqueueLoad(component) {
 }
 
 module.exports = Component => class extends mix(Component).with(viewportCollisionMixin) {
-    constructor(opts) {
-        super(defaults(opts, {
-            inputs: ['src', 'width', 'height'],
-            state: {
-                hasLoaded: false,
-                errored: false,
-                canLoad: false,
-                aspectRatio: function() {
-                    return !isNaN(this.height) && !isNaN(this.width) ? this.height / this.width : 0;
-                },
-                wasInViewport: false,
-                width: null,
-                height: null,
-                currentSrc: function(){
-                    return this.hasLoaded || this.canLoad ? this.src : null;
-                }
+
+    static get state() {
+        return defaults({
+            hasLoaded: false,
+            errored: false,
+            canLoad: false,
+            aspectRatio: function () {
+                return !isNaN(this.height) && !isNaN(this.width) ? this.height / this.width : 0;
             },
-            markupTemplate: require('./index.pug'),
-            stylesFormat: '(locals)=>CSSString',
-			stylesTemplate: (locals)=>{
-                return `
-                    img.lazy-img.has-loaded {
-                        animation: lazy-img-fade-in;
-                        animation-duration: 0.5s;
-                        animation-fill-mode: forwards;
-                    }
-
-                    .lazy-img.loading img {
-                        max-height: 0;
-                    }
-                    
-                    img.lazy-img:not(.has-loaded) {
-                        width: 0;
-                        height: 0;
-                    }
-
-                    .lazy-img.placeholder::before {
-                        content: '';
-                        display: block;
-                    }
-
-                    #lazy-img-${locals.$id}.placeholder::before {
-                        padding-top: ${100 * (locals.aspectRatio)}%;
-                    }
-
-                    @keyframes lazy-img-fade-in {
-                        from {
-                            opacity: 0;
-                        }  
-                        to {
-                            opacity: 1;
-                        }
-                    }
-                `
+            wasInViewport: false,
+            width: null,
+            height: null,
+            currentSrc: function () {
+                return this.hasLoaded || this.canLoad ? this.src : null;
             }
-        }));
+        }, super.state);
     }
 
-    onInit() {
-        this.state.watch('src', src => {
-            resetDone(this);
-            this.state.hasLoaded = false;
-            this.state.errored = false;
-            this.state.wasInViewport = this.state.isInViewport;
-            if (this.state.isInViewport) {
-                queueLoad(this);
-            }
-        })
+    static get markup() {
+        return require('./index.pug');
+    }
 
-        this.state.watch('isInViewport', isInViewport => {
-            if (isInViewport) {
-                this.state.wasInViewport = true;
-                queueLoad(this);
-            } else {
-                unqueueLoad(this);
-            }
-        })
+    static get inputs() {
+        return ['src', 'width', 'height']
+    }
+
+    static get styles() {
+        return [
+            require('./index.css'),
+            locals => `
+                #lazy-img-${locals.$id}.placeholder::before {
+                    padding-top: ${100 * (locals.aspectRatio)}%;
+                }
+            `
+        ]
+    }
+
+    static get watchers() {
+        return [
+            [
+                'src', function (src) {
+                    resetDone(this);
+                    this.hasLoaded = false;
+                    this.errored = false;
+                    this.wasInViewport = this.isInViewport;
+                    if (this.isInViewport) {
+                        queueLoad(this);
+                    }
+                }
+            ],
+            [
+                'isInViewport', function (isInViewport) {
+                    if (isInViewport) {
+                        this.wasInViewport = true;
+                        queueLoad(this);
+                    } else {
+                        unqueueLoad(this);
+                    }
+                }
+            ]
+        ]
+    }
+
+    async onMount() {
+        await super.onMount()
+        this.getParent().on('lazyloadimages', this.loadCallback = () => this.checkViewportCollision())
+    }
+
+    async onUnmount() {
+        await super.onUnmount()
+        this.getParent().off('lazyloadimages', this.loadCallback)
     }
 
     markLoaded() {
         this.state.hasLoaded = true;
-        markDone(this);
+        markDone(this.state);
     }
 
     markErrored() {
         this.state.errored = true;
-        markDone(this);
+        markDone(this.state);
     }
 }
